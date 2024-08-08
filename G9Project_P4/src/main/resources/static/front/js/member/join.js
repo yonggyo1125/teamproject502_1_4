8899383d0197cbd0cbb24aadbae7bde1507ad099
yonggyo1125 <yonggyo00@kakao.com>
2024-08-08 12:36:02 +0900

    프로필 업로드

--- G9Project_P4/src/main/resources/static/front/js/member/join.js
+++ G9Project_P4/src/main/resources/static/front/js/member/join.js
@@ -3,5 +3,31 @@
 *
 */
 function fileUploadCallback(files) {
-    console.log(files);
+    if (files.length === 0) {
+        return;
+    }
+
+    const file = files[0];
+    let html = document.getElementById("image-file-tpl").innerHTML;
+    html = html.replace(/\[seq\]/g, file.seq)
+                .replace(/\[fileUrl\]/g, file.fileUrl);
+
+    const domParser = new DOMParser();
+    const dom = domParser.parseFromString(html, 'text/html');
+    const box = dom.querySelector(".image-file-box");
+
+    const targetEl = document.querySelector(".profile-image");
+    targetEl.append(box);
+
+    const removeEl = box.querySelector(".remove");
+    removeEl.addEventListener("click", function() {
+        if (!confirm('정말 삭제 하겠습니까?')) {
+            return;
+        }
+
+        const seq = this.dataset.seq;
+        fileManager.delete(seq);
+    });
+
+
 }
