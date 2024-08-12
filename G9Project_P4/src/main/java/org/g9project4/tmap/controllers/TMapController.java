6a2a8128ecbf596f73c1d1ce8c49e16338e2d0eb
yonggyo1125 <yonggyo00@kakao.com>
2024-08-12 14:48:16 +0900

    1조 지원

--- G9Project_P4/src/main/java/org/g9project4/tmap/controllers/TMapController.java
+++ G9Project_P4/src/main/java/org/g9project4/tmap/controllers/TMapController.java
@@ -1,18 +1,33 @@
 package org.g9project4.tmap.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.g9project4.global.Utils;
 import org.g9project4.global.services.ApiConfigService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller("/tmap")
 @RequiredArgsConstructor
 public class TMapController {
-    private final ApiConfigService apiConfigService;
 
+    private final ApiConfigService apiConfigService;
+    private final Utils utils;
 
     @ModelAttribute("tmapJavascriptKey")
     public String tmapJavascriptKey() {
         return apiConfigService.get("tmapJavascriptKey");
+    }
+
+    @GetMapping("/test")
+    public String tmapTest(Model model) {
+
+        model.addAttribute("addCommonScript", List.of("tmap"));
+        model.addAttribute("addScript", List.of("tmap/test"));
+
+        return utils.tpl("tmap/test");
     }
 }
