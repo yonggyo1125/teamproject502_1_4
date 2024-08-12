2bcfee51656108c3bc4f1c00855a804aff96e301
yonggyo1125 <yonggyo00@kakao.com>
2024-08-12 16:01:00 +0900

    1조 지원

--- G9Project_P4/src/main/resources/static/common/js/tmap.js
+++ G9Project_P4/src/main/resources/static/common/js/tmap.js
@@ -1,4 +1,6 @@
 const tmapLib = {
+    departure: null, // 출발지 LatLng 객체
+    arrival: null, // 도착지 latLng 객체
     load(mapId, width, height, zoom) {
         navigator.geolocation.getCurrentPosition((pos) => {
             const { latitude, longitude } = pos.coords;
@@ -13,6 +15,22 @@ const tmapLib = {
                 scrollwheel : true
             });
 
+            /**
+            * 지도 좌표 클릭 처리
+            *
+            * 좌표는 총 2개 - 출발지 좌표, 도착 좌표
+            *       출발지 좌표, 도착 좌표가 모두 체크된 경우 -> 경로 표기
+            */
+            map.addListener("click", function(e) {
+                if (!tmapLib.departure) { // 출발지 좌표 객체
+                    tmapLib.departure = e.latLng;
+
+                } else if (tmapLib.departure && !tmapLib.arrival) { // 도착지 좌표
+                    tmapLib.arrival = e.latLng;
+                }
+
+                console.log(tmapLib);
+            });
         });
     }
 };
