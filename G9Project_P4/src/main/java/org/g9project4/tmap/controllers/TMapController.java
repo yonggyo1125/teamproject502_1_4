5013266dd65dca2ee1348112c4605fa71915415c
yonggyo1125 <yonggyo00@kakao.com>
2024-08-12 14:38:21 +0900

    1조 지원

---
+++ G9Project_P4/src/main/java/org/g9project4/tmap/controllers/TMapController.java
@@ -0,0 +1,7 @@
+package org.g9project4.tmap.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/tmap")
+public class TMapController {
+}
--- admin/src/main/java/org/g9project4/config/controllers/ApiConfig.java
+++ admin/src/main/java/org/g9project4/config/controllers/ApiConfig.java
@@ -8,4 +8,6 @@ public class ApiConfig {
 
     private String kakaoJavascriptKey; // 카카오 API - 자바스크립트 앱 키
     private String kakaoRestApiKey; // 카카오 API - REST API 키
+    
+    private String tmapJavascriptKey; // TMAP API 자바스크립트 앱 키
 }
