8d79224f1ab356c93d8a61d2d30a2e7ee01f9de3
yonggyo1125 <yonggyo00@kakao.com>
2024-08-13 10:48:59 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/board/controllers/RequestBoard.java
+++ G9Project_P4/src/main/java/org/g9project4/board/controllers/RequestBoard.java
@@ -10,6 +10,9 @@ public class RequestBoard {
     private String mode = "write"; // write : 글 작성, update - 글 수정
     private String bid; // 게시판 ID
     private String gid = UUID.randomUUID().toString();
+
+    private boolean notice; // 공지글 여부
+
     private String category;
     private String poster;
     private String subject;
