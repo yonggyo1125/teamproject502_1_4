fd3530ef8d28745d2b22e4cf0c87eb8f68896c33
yonggyo1125 <yonggyo00@kakao.com>
2024-08-13 17:57:03 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/global/configs/BeanConfig.java
+++ G9Project_P4/src/main/java/org/g9project4/global/configs/BeanConfig.java
@@ -1,8 +1,22 @@
 package org.g9project4.global.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfig {
-    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        return om;
+    }
 }
--- G9Project_P4/src/main/java/org/g9project4/payment/services/PaymentProcessService.java
+++ G9Project_P4/src/main/java/org/g9project4/payment/services/PaymentProcessService.java
@@ -1,13 +1,22 @@
 package org.g9project4.payment.services;
 
 import lombok.RequiredArgsConstructor;
+import org.g9project4.global.SHA256;
 import org.g9project4.global.Utils;
 import org.g9project4.payment.controllers.PayAuthResult;
 import org.g9project4.payment.exceptions.PaymentAuthException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +36,11 @@ public class PaymentProcessService {
         String authUrl = result.getAuthUrl();
         String netCancelUrl = result.getNetCancelUrl();
 
+        PaymentConfig config = configService.get();
+        String signKey = config.getSignKey();
+
+        long timestamp = new Date().getTime();
+
         // 인증 실패, 승인 실패시 이동할 주소
         String orderNumber = result.getOrderNumber();
         String redirectUrl = utils.redirectUrl("/order/order_fail?orderNo=" + orderNumber);
@@ -38,9 +52,36 @@ public class PaymentProcessService {
             throw new PaymentAuthException(result.getResultMsg(), redirectUrl);
         }
 
+
+
         // 요청 데이터
+
+        String signature = "";
+        String verification = "";
+        try {
+            signature = SHA256.encrypt(String.format("authToken=%s&timestamp=%d", authToken, timestamp));
+            verification = SHA256.encrypt(String.format("authToken=%s&signKey=%s&timestamp=%d", authToken, signKey, timestamp));
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("mid", mid);
+        params.add("authToken", authToken);
+        params.add("timestamp", String.valueOf(timestamp));
+        params.add("signature", signature);
+        params.add("verification", verification);
+        params.add("charset", "UTF-8");
+        params.add("format", "JSON");
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(URI.create(authUrl), request, String.class);
+
+        System.out.println(response);
     }
 }
