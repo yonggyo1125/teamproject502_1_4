f053c16aaeeedd87dc59e457866479ce6e31ad40
yonggyo1125 <yonggyo00@kakao.com>
2024-08-13 16:01:23 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/order/services/PaymentConfigService.java
+++ G9Project_P4/src/main/java/org/g9project4/order/services/PaymentConfigService.java
@@ -2,17 +2,45 @@ package org.g9project4.order.services;
 
 import lombok.RequiredArgsConstructor;
 import org.g9project4.config.services.ConfigInfoService;
+import org.g9project4.global.SHA256;
 import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentConfigService {
     private final ConfigInfoService infoService;
 
-    public PaymentConfig get() {
+    public PaymentConfig get(long oid, int price) {
         PaymentConfig config = infoService.get("payment", PaymentConfig.class)
                 .orElseGet(PaymentConfig::new);
+        long timestamp = new Date().getTime();
+        String signKey = config.getSignKey();
 
+        try {
+            // signature S
+            String data = String.format("oid=%d&price=%d&timestamp=%d", oid, price, timestamp);
+            String signature = SHA256.encrypt(data);
+            config.setSignature(signature);
+            // signature E
+
+            // verification S
+            data = String.format("oid=%d&price=%d&signKey=%s&timestamp=%d", oid, price, signKey, timestamp);
+            String verification = SHA256.encrypt(data);
+            config.setVerification(verification);
+            // verification E
+
+            // mKey
+            String mKey = SHA256.encrypt(signKey);
+            config.setMKey(mKey);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        config.setTimestamp(timestamp);
         return config;
     }
 
