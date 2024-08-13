d99008444f38172d94af8e4bfb8812ce03c01699
yonggyo1125 <yonggyo00@kakao.com>
2024-08-13 11:48:44 +0900

    4조 지원

---
+++ G9Project_P4/src/main/java/org/g9project4/board/exceptions/BoardNotFoundException.java
@@ -0,0 +1,11 @@
+package org.g9project4.board.exceptions;
+
+import org.g9project4.global.exceptions.script.AlertBackException;
+import org.springframework.http.HttpStatus;
+
+public class BoardNotFoundException extends AlertBackException {
+    public BoardNotFoundException() {
+        super("NotFound.board", HttpStatus.NOT_FOUND);
+        setErrorCode(true);
+    }
+}
