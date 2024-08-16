c81b25024b7be6c63eaa85a107eed5bdcd5f1986
yonggyo1125 <yonggyo00@kakao.com>
2024-08-16 12:34:58 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/board/services/BoardConfigInfoService.java
+++ G9Project_P4/src/main/java/org/g9project4/board/services/BoardConfigInfoService.java
@@ -1,8 +1,10 @@
 package org.g9project4.board.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.g9project4.board.entities.Board;
 import org.g9project4.global.Utils;
+import org.g9project4.global.rests.JSONData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BoardConfigInfoService {
     private final RestTemplate restTemplate;
+    private final ObjectMapper om;
     private final Utils utils;
 
     /**
@@ -23,9 +26,18 @@ public class BoardConfigInfoService {
     public Board get(String bid) {
         try {
             String url = utils.adminUrl("/api/board/config/" + bid);
-            ResponseEntity<Board> response = restTemplate.getForEntity(url, Board.class);
+            ResponseEntity<JSONData> response = restTemplate.getForEntity(url, JSONData.class);
             if (response.getStatusCode().isSameCodeAs(HttpStatus.OK)) {
-                return response.getBody();
+                JSONData jsonData = response.getBody();
+                if (!jsonData.isSuccess()) {
+                    return null;
+                }
+
+                Object data = jsonData.getData();
+
+                Board board = om.readValue(om.writeValueAsString(data), Board.class);
+
+                return board;
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ G9Project_P4/src/test/java/org/g9project4/board/services/BoardConfigInfoServiceTest.java
@@ -0,0 +1,16 @@
+package org.g9project4.board.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardConfigInfoServiceTest {
+    @Autowired
+    private BoardConfigInfoService infoService;
+
+    @Test
+    void boardConfigTest() {
+
+    }
+}
