1c74ac80c48316a0c0b85062256be17eea4dd39c
yonggyo1125 <yonggyo00@kakao.com>
2024-08-16 09:11:45 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/board/services/BoardInfoService.java
+++ G9Project_P4/src/main/java/org/g9project4/board/services/BoardInfoService.java
@@ -1,4 +1,39 @@
 package org.g9project4.board.services;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.g9project4.board.entities.BoardData;
+import org.g9project4.board.repositories.BoardDataRepository;
+import org.g9project4.global.ListData;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BoardInfoService {
+
+    private final JPAQueryFactory queryFactory;
+    private final BoardDataRepository repository;
+
+    /**
+     * 게시글 목록 조회
+     *
+     * @return
+     */
+    private ListData<BoardData> getList() {
+
+        return null;
+    }
+
+    /**
+     * 게시글 개별 조회
+     *
+     * @param seq
+     * @return
+     */
+    private BoardData get(Long seq) {
+
+        return null;
+    }
 }
--- G9Project_P4/src/main/java/org/g9project4/global/configs/BeanConfig.java
+++ G9Project_P4/src/main/java/org/g9project4/global/configs/BeanConfig.java
@@ -2,12 +2,24 @@ package org.g9project4.global.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
