675c84f396f1782bd21a8d44686fffe8d2f6d8ad
yonggyo1125 <yonggyo00@kakao.com>
2024-08-19 11:50:40 +0900

    4조 지원

--- G9Project_P4/src/main/resources/static/front/js/board/default/form.js
+++ G9Project_P4/src/main/resources/static/front/js/board/default/form.js
@@ -7,3 +7,31 @@ window.addEventListener("DOMContentLoaded", function() {
         } catch (err) {}
    })();
 });
+
+
+/**
+* 파일 업로드 후속 처리
+*
+* files : 업로드한 파일 목록
+*/
+function fileUploadCallback(files) {
+    if (!files || files.length === 0) {
+        return;
+    }
+
+    // 에디터에 첨부할 이미지 URL
+    const imageUrls = [];
+
+    for (const file of files) {
+        const { location, fileUrl } = file;
+
+        if (location === 'editor') { // 에디터 첨부
+            imageUrls.push(fileUrl);
+        }
+    }
+
+    // 에디터 본문에 이미지 추가
+    if (imageUrls.length > 0) {
+        editor.execute("insertImage", { source: imageUrls });
+    }
+}
