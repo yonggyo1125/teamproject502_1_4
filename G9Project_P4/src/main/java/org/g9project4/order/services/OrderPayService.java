7d6cf128d0462f3e8c9d3db5c7d11435370fcb30
yonggyo1125 <yonggyo00@kakao.com>
2024-08-19 16:56:31 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/order/services/OrderPayService.java
+++ G9Project_P4/src/main/java/org/g9project4/order/services/OrderPayService.java
@@ -1,6 +1,8 @@
 package org.g9project4.order.services;
 
 import lombok.RequiredArgsConstructor;
+import org.g9project4.order.entities.OrderInfo;
+import org.g9project4.order.entities.OrderItem;
 import org.g9project4.payment.services.PaymentConfig;
 import org.g9project4.payment.services.PaymentConfigService;
 import org.springframework.stereotype.Service;
@@ -8,11 +10,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderPayService {
-
+    private final OrderInfoService orderInfoService;
     private final PaymentConfigService paymentConfigService;
 
     public PaymentConfig getConfig(Long orderNo) {
+        OrderInfo orderInfo = orderInfoService.get(orderNo);
+        PaymentConfig config = paymentConfigService.get(orderNo, orderInfo.getTotalPayPrice());
 
-        return null;
+        OrderItem orderItem = orderInfo.getOrderItem();
+        String goodName = orderItem.getItemName();
+        int qty = orderItem.getQty();
+        if (qty > 1) goodName += " X " + qty + "개";
+
+        config.setOid(orderNo);
+        config.setPrice(orderInfo.getTotalPayPrice());
+        config.setGoodname(goodName);
+        config.setBuyertel(orderInfo.getOrderMobile());
+        config.setBuyeremail(orderInfo.getOrderEmail());
+
+        return config;
     }
 }
