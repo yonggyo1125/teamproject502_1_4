600680ca7dea1d9b012d2cc3194e1d056632390b
yonggyo1125 <yonggyo00@kakao.com>
2024-08-19 11:33:00 +0900

    4조 지원

--- G9Project_P4/src/main/resources/static/front/js/board/default/form.js
+++ G9Project_P4/src/main/resources/static/front/js/board/default/form.js
@@ -1,8 +1,9 @@
 window.addEventListener("DOMContentLoaded", function() {
-    if (ClassicEditor) { // 에디터 사용의 경우
-        ClassicEditor.create(document.getElementById("content"), {
-            height: 450,
-        })
-        .then((editor) => console.log(editor));
-    }
+   (async() => {
+        try {
+            const { editorLoad } = commonLib;
+            const editor = await editorLoad("content");
+            if (editor) window.editor = editor;
+        } catch (err) {}
+   })();
 });
