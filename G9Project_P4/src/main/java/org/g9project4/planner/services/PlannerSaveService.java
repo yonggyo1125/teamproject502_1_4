a6883f8919de439e7e3e4bf910bcb8a4e7b112e2
yonggyo1125 <yonggyo00@kakao.com>
2024-08-22 19:47:51 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerSaveService.java
+++ G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerSaveService.java
@@ -2,6 +2,8 @@ package org.g9project4.planner.services;
 
 import lombok.RequiredArgsConstructor;
 import org.g9project4.member.MemberUtil;
+import org.g9project4.member.entities.Member;
+import org.g9project4.member.repositories.MemberRepository;
 import org.g9project4.planner.controllers.RequestPlanner;
 import org.g9project4.planner.entities.Planner;
 import org.g9project4.planner.exceptions.PlannerNotFoundException;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PlannerSaveService {
     private final PlannerRepository repository;
+    private final MemberRepository memberRepository;
     private final MemberUtil memberUtil;
 
     public Planner save(RequestPlanner form) {
@@ -21,8 +24,11 @@ public class PlannerSaveService {
         if (mode.equals("update")) { // 수정
             planner = repository.findById(seq).orElseThrow(PlannerNotFoundException::new);
         } else { // 추가
+
+            Member member = memberUtil.isLogin() ? memberRepository.findByEmail(memberUtil.getMember().getEmail()).orElse(null) : null;
+
             planner = Planner.builder()
-                    .member(memberUtil.getMember())
+                    .member(member)
                     .build();
         }
 
