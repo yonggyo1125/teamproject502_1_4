1d375aadd45129fefa39d38da7cc9936950fa62d
yonggyo1125 <yonggyo00@kakao.com>
2024-08-23 10:27:39 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/wishlist/services/WishListService.java
+++ G9Project_P4/src/main/java/org/g9project4/wishlist/services/WishListService.java
@@ -2,7 +2,10 @@ package org.g9project4.wishlist.services;
 
 import lombok.RequiredArgsConstructor;
 import org.g9project4.member.MemberUtil;
+import org.g9project4.wishlist.constants.WishType;
 import org.g9project4.wishlist.entities.WishList;
+import org.g9project4.wishlist.entities.WishListId;
+import org.g9project4.wishlist.repositories.WishListRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,13 +14,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WishListService {
     private final MemberUtil memberUtil;
+    private final WishListRepository repository;
 
-    public void add(Long seq, WishList type) {
+    public void add(Long seq, WishType type) {
+        if (!memberUtil.isLogin()) {
+            return;
+        }
 
+        WishList wishList = WishList.builder()
+                .wishType(type)
+                .seq(seq)
+                .member(memberUtil.getMember())
+                .build();
+        repository.saveAndFlush(wishList);
     }
 
-    public void remove(Long seq, WishList type) {
+    public void remove(Long seq, WishType type) {
+        if (!memberUtil.isLogin()) {
+            return;
+        }
 
+        WishListId wishListId = new WishListId(seq, type, memberUtil.getMember());
+        repository.deleteById(wishListId);
+        repository.flush();
     }
 
     public List<Long> getList(WishList type) {
