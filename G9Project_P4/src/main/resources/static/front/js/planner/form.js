092f00360ded9e9ae216641a3dd58eafd474688a
yonggyo1125 <yonggyo00@kakao.com>
2024-08-23 20:19:46 +0900

    4조 지원

--- G9Project_P4/src/main/resources/static/front/js/planner/form.js
+++ G9Project_P4/src/main/resources/static/front/js/planner/form.js
@@ -1,3 +1,15 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const reSelectCalendar = document.getElementById("reselect-calendar");
+    reSelectCalendar.addEventListener("click", function() {
+        // 선택된 날짜 hidden 값 - sDate, eDate 값을 초기화
+        frmSave.sDate.value = frmSave.eDate.value = "";
+
+        // 달력 갱신 /calendar
+        const rootUrl = document.querySelector("meta[name='rootUrl']").content;
+        const url = `${rootUrl}calendar`;
+        ifrmCalendar.location.href = url;
+    });
+});
 
 
 /**
