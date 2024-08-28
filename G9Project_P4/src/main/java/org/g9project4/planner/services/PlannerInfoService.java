a84926106002f41cd4d61cdb44c2769732afd284
yonggyo1125 <yonggyo00@kakao.com>
2024-08-28 15:35:49 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerInfoService.java
+++ G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerInfoService.java
@@ -10,7 +10,14 @@ import org.g9project4.planner.entities.Planner;
 import org.g9project4.planner.entities.QPlanner;
 import org.g9project4.planner.exceptions.PlannerNotFoundException;
 import org.g9project4.planner.repositories.PlannerRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import static org.springframework.data.domain.Sort.Order.desc;
 
 @Service
 @RequiredArgsConstructor
@@ -49,6 +56,39 @@ public class PlannerInfoService {
     }
 
     public ListData<Planner> getList(PlannerSearch search) {
+
+        if (!memberUtil.isLogin()) {
+            return new ListData<>();
+        }
+
+        int page = Math.max(search.getPage(), 1);
+        int limit = search.getLimit();
+        limit = limit < 1 ? 20 : limit;
+
+        /* 검색 처리 S */
+
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        QPlanner planner = QPlanner.planner;
+        if (!memberUtil.isAdmin()) {
+            andBuilder.and(planner.member.seq.eq(memberUtil.getMember().getSeq()));
+        }
+
+        String sopt = search.getSopt();
+        String skey = search.getSkey();
+
+        sopt = StringUtils.hasText(sopt) ? sopt : "ALL";
+        if (skey != null && !skey.isBlank()) {
+            skey = skey.trim();
+            if (sopt.equals("ALL")) {
+                andBuilder.and(planner.title.contains(skey));
+            }
+        }
+        /* 검색 처리 E */
+
+        Pageable pageable = PageRequest.of(page - 1, limit, Sort.by(desc("createdAt")));
+        Page<Planner> data = plannerRepository.findAll(andBuilder, pageable);
+
+
         return null;
     }
 
