a2e68da41a491de458dd54491d85f654c06bbc0c
yonggyo1125 <yonggyo00@kakao.com>
2024-08-28 15:26:50 +0900

    4조 지원

---
+++ G9Project_P4/src/main/java/org/g9project4/planner/controllers/PlannerSearch.java
@@ -0,0 +1,6 @@
+package org.g9project4.planner.controllers;
+
+import org.g9project4.global.CommonSearch;
+
+public class PlannerSearch extends CommonSearch {
+}
--- G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerInfoService.java
+++ G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerInfoService.java
@@ -1,8 +1,13 @@
 package org.g9project4.planner.services;
 
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.g9project4.global.ListData;
+import org.g9project4.global.exceptions.UnAuthorizedException;
+import org.g9project4.member.MemberUtil;
+import org.g9project4.planner.controllers.PlannerSearch;
 import org.g9project4.planner.entities.Planner;
+import org.g9project4.planner.entities.QPlanner;
 import org.g9project4.planner.exceptions.PlannerNotFoundException;
 import org.g9project4.planner.repositories.PlannerRepository;
 import org.springframework.stereotype.Service;
@@ -11,7 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PlannerInfoService {
     private final PlannerRepository plannerRepository;
-
+    private final MemberUtil memberUtil;
     /**
      * 플래너 한개 조회
      *
@@ -19,19 +24,31 @@ public class PlannerInfoService {
      * @return
      */
     public Planner get(Long seq) {
-        Planner planner = plannerRepository.findById(seq).orElseThrow(PlannerNotFoundException::new);
+        if (!memberUtil.isLogin()) {
+            throw new UnAuthorizedException();
+        }
+
+        QPlanner planner = QPlanner.planner;
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(planner.seq.eq(seq));
+
+        if (!memberUtil.isAdmin()) {
+            builder.and(planner.member.seq.eq(memberUtil.getMember().getSeq()));
+        }
+
+        Planner item = plannerRepository.findOne(builder).orElseThrow(PlannerNotFoundException::new);
 
         /**
          *   추가 처리
          *   1. 여행 노트 목록 ...
           */
 
-        addInfo(planner);
+        addInfo(item);
 
-        return planner;
+        return item;
     }
 
-    public ListData<Planner> getList() {
+    public ListData<Planner> getList(PlannerSearch search) {
         return null;
     }
 
