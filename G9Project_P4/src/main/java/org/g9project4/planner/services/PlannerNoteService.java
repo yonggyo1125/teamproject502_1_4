4f5c90148ddc09356c780e66e02c095dccbf0f08
yonggyo1125 <yonggyo00@kakao.com>
2024-08-28 20:49:52 +0900

    4조 지원

--- G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerNoteService.java
+++ G9Project_P4/src/main/java/org/g9project4/planner/services/PlannerNoteService.java
@@ -2,6 +2,10 @@ package org.g9project4.planner.services;
 
 import lombok.RequiredArgsConstructor;
 import org.g9project4.board.controllers.RequestBoard;
+import org.g9project4.file.constants.FileStatus;
+import org.g9project4.file.entities.FileInfo;
+import org.g9project4.file.services.FileInfoService;
+import org.g9project4.global.Utils;
 import org.g9project4.global.exceptions.script.AlertBackException;
 import org.g9project4.planner.entities.Planner;
 import org.springframework.http.HttpStatus;
@@ -10,10 +14,17 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class PlannerNoteService {
     private final PlannerInfoService infoService;
+    private final FileInfoService fileInfoService;
+    private final Utils utils;
+
     /**
      * 여행 노트 게시판 공통 처리 부분
      *
@@ -34,12 +45,29 @@ public class PlannerNoteService {
         }
 
         String content = form.getContent();
+        if (StringUtils.hasText(content)) {
+            Map<String, String> contents = utils.toMap(content);
+            Map<String, List<FileInfo>> files = new HashMap<>();
+            for (Map.Entry<String, String> entry : contents.entrySet()) {
+                String key = entry.getKey();
+                String location = "editor_" + key.split("_")[1];
+                List<FileInfo> fileItems = fileInfoService.getList(form.getGid(), location, FileStatus.ALL);
+                files.put(location, fileItems);
+            }
+
+            model.addAttribute("contents", contents);
+            model.addAttribute("files", files);
+        }
+
 
     }
 
     public void commonProcess(RequestBoard form, Errors errors, Model model) {
 
         // 추가 검증 처리
+        if (errors.hasErrors()) {
+
+        }
 
         commonProcess(form, model);
     }
