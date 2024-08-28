eb71f47a81a5029ebc1bcc02cf67b7e1609ab290
yonggyo1125 <yonggyo00@kakao.com>
2024-08-28 20:07:09 +0900

    4조 지원

--- G9Project_P4/src/main/resources/static/front/js/board/planner/form.js
+++ G9Project_P4/src/main/resources/static/front/js/board/planner/form.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", function() {
     const contentEls = document.getElementsByClassName("content");
     for (const el of contentEls) {
         const editorName = `editor_${el.dataset.seq}`;
-        (async() {
+        (async() => {
             try {
                 const editor = await editorLoadByDOM(el);
                 window.editors = window.editors ?? {}
@@ -17,4 +17,26 @@ window.addEventListener("DOMContentLoaded", function() {
 
 function getEditor(editorName) {
     return editors[editorName];
+}
+
+
+/** 이미지 업로드 후속 처리 */
+function fileUploadCallback(files) {
+    if (!files || files.length === 0) return;
+
+    const imageUrls = {};
+    for (const file of files) {
+        const { location, fileUrl } = file;
+        imageUrls[location] = imageUrls[location] ?? [];
+        imageUrls[location].push(fileUrl);
+    }
+
+    insertImages(imageUrls);
+}
+
+function insertImages(imageUrls) {
+    for (const [editorName, urls] of Object.entries(imageUrls)) {
+        const editor = getEditor(editorName);
+        editor.execute('insertImage', { source: urls });
+    }
 }
